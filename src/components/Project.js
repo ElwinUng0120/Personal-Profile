b547e09438b0059f77d62d5093e6fbadfe7f1558
Elwin Ung <elwin.ung@gmail.com>
2020-11-18 18:45:06 -0500

    Changed NavLink to a in Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
 
 function Project(){
     return (
     <>
-        <NavLink to="https://github.com/ElwinUng0120/README-Generator" target="blank">
+        <a href="https://github.com/ElwinUng0120/README-Generator" target="blank">
             <img className="hoverZoom" src="http://placehold.it/500x300" alt="readme_generator.jpg" value="1"/>
-        </NavLink>
+        </a>
         <br/><br/>
     </>
     );
